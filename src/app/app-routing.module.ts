068630111f871031f185d42ad0a399da502f79b8
Ricardo Benjamín Paz González <89035320+rpaz2017457@users.noreply.github.com>
2022-07-29 22:58:38 -0600

    code changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';;
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { UsuarioGuard } from './services/usuario.guard';
+import { AdminGuard } from './services/admin.guard';
+import { InicioUsuarioComponent } from './components/inicio-usuario/inicio-usuario.component';
+import { InicioAdminComponent } from './components/inicio-admin/inicio-admin.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar', component: RegistrarComponent },
+  { path: 'inicio', component: InicioComponent },
+  {
+    path: 'usuario',
+    component: InicioUsuarioComponent,
+    canActivate: [UsuarioGuard],
+    children: [
+      { path: 'inicio', component: InicioComponent },
+      { path: 'servicios/:idHotel', component: ServiciosComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: InicioAdminComponent,
+    canActivate: [AdminGuard],
+    children: [
+      { path: 'inicio', component: InicioComponent },
+      { path: 'servicios/:idHotel', component: ServiciosComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
